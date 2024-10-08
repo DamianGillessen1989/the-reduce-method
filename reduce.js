b375aa196652d870a87e324360dd2f6a5e03cc35
Damian Gillessen <damian.gillessen.2009@alumni.nuim.ie>
2024-10-07 18:16:30 -0600

    Initial commit

--- reduce.js
+++ reduce.js
@@ -1,4 +1,4 @@
-  
+
 /**
  * To run this file in Gitpod, use the 
  * command node reduce.js in the terminal
@@ -6,7 +6,23 @@
 
 
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => {
+  console.log(
+    "Accumulator:", acc,
+    "Current Value:", curr,
+    "Total:", acc + curr
+  );
+    return acc + curr
+}, 10);
 
+console.log(sum);
+
+// or
+
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
 
 const teamMembers = [
   {
@@ -32,6 +48,18 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
-
+let totalExprience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExprience);
 
 // Grouping by a property, and totaling it too
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(experienceByProfession);
